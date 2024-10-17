1f7ec914b52fbada195a3a7c462d9d597fa21d35
emmanuel irandi <emmanuelirandi@emmanuelirandi.home>
2024-10-17 17:45:56 +0100

    finished ticket 11

--- models/articlesModel.js
+++ models/articlesModel.js
@@ -1,9 +1,10 @@
 const db = require("../db/connection");
 
-const selectArticles = ()=>{
-    
+const selectArticles = (sort_by = "created_at",order = "DESC")=>{
     return db.query(`
         SELECT * FROM articles
+        ORDER BY ${sort_by} ${order}
+
         `)
         .then((result)=>{
             return result.rows
