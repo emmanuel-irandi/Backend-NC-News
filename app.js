06cacb2fe95cc3e0681ec1e66227d144ff1cad84
emmanuel irandi <emmanuelirandi@emmanuelirandi.home>
2024-10-17 16:00:43 +0100

    Ticket 7

--- app.js
+++ app.js
@@ -5,12 +5,16 @@ const getEndPoint = require('./controllers/controllerTwo')
 const getId = require('./controllers/controllerThree')
 const getArticles = require('./controllers/articlesController');
 const getComments = require('./controllers/commentController');
+const postComment = require('./controllers/postCommentController');
 
+app.use(express.json());
 
 app.get("/api/topics",getTopics)
 app.get("/api",getEndPoint)
 app.get("/api/articles/:article_id",getId);
 app.get("/api/articles", getArticles);
 app.get("/api/articles/:article_id/comments",getComments);
+
+app.post("/api/articles/:article_id/comments",postComment);
 
 module.exports = app;
